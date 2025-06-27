63606be3086cca42b569b8f56fec48d5ab1783fc
rusrom <19729227+rusrom@users.noreply.github.com>
2025-06-27 20:56:22 +0300

    Generated dags based on Dynamic DAG

---
+++ dags/get_price_appl.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from airflow.decorators import task
+from datetime import datetime
+
+
+with DAG(
+    dag_id="dynamic_dag_get_price_appl",
+    start_date=datetime(2025, 6, 27),
+    schedule="@daily",
+    catchup=False,
+) as dag:
+    @task
+    def extract(stock):
+        print("Extracting stock ...")
+        return stock
+
+    @task
+    def process(stock):
+        print("Processing stock ...")
+        return stock
+
+
+    @task
+    def send_email(stock):
+        print(f"Sending email with stock: {stock}")
+        return stock
+
+    send_email(process(extract("123")))
