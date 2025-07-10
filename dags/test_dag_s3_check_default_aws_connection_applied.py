88ac858bebd1e0222309c5ad98535cf414ea9a13
romblox <156704248+romblox@users.noreply.github.com>
2025-07-10 20:16:10 +0300

    Implement AWS provider default connection (#2)

---
+++ dags/test_dag_s3_check_default_aws_connection_applied.py
@@ -0,0 +1,31 @@
+from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor
+from airflow.sdk import dag, task
+
+
+# from airflow.operators.dummy import DummyOperator
+
+
+@dag(
+    dag_id='s3_access_check_on_default_aws_connection_applied',
+    description='Check access to S3 bucket on default AWS connection',
+    tags=['s3', 'access', 'check'],
+)
+def s3_access_check_on_default_aws_connection_applied():
+    @task
+    def start_check():
+        print("Start checking S3 access")
+
+    check_s3_access = S3KeySensor(
+        task_id='check_s3_access',
+        bucket_key='data_sources/',
+        bucket_name='s3-shkiper-private',
+    )
+
+    @task
+    def end_check():
+        print("Finished checking S3 access")
+
+    start_check() >> check_s3_access >> end_check()
+
+
+s3_access_check_on_default_aws_connection_applied()
