5d4072bcc73a2b81ea1967d207dcf8ec5e8f675e
rusrom <19729227+rusrom@users.noreply.github.com>
2025-07-10 11:23:17 +0300

    Add Celery DAG using several queues

---
+++ dags/celery.py
@@ -0,0 +1,41 @@
+from time import sleep
+
+from airflow.sdk import dag, task
+
+
+# @dag(
+#     schedule=None,
+#     start_date=None,
+#     catchup=False,
+# )
+@dag
+def celery_test_dag():
+    @task
+    def a():
+        sleep(2)
+
+    @task
+    def b():
+        sleep(5)
+
+    @task(queue="high_cpu_queue")
+    def c():
+        sleep(5)
+
+    @task()
+    def d():
+        sleep(5)
+
+    @task
+    def e():
+        sleep(5)
+
+    @task(queue="high_cpu_queue")
+    def f():
+        sleep(2)
+
+
+
+    a() >> [b(), c(), d(), e()] >> f()
+
+celery_test_dag()
